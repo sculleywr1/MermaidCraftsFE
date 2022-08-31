a889eafe3f75e37d5dd926d239db2475dfc1a454
sculleywr1 <sculleywr1@gmail.com>
2022-08-31 08:27:17 -0500

    Add project files.

---
+++ MermaidCraftsFE/Server/Data/DataContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MermaidCraftsFE.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Teddy Bear",
+                    Description = "A teddy bear made with baby soft yarn, perfect for your child",
+                    ImageUrl = "https://images.squarespace-cdn.com/content/v1/5e12aa9f8c03f756cbec18fc/1607712216490-NN3SB29TDH8A2RP24HLY/classic-crochet-teddy-bear.jpg?format=1000w",
+                    Price = 50.00m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Baby Shirt",
+                    Description = "A shirt made with baby soft yarn in sizes from 2t-5t",
+                    ImageUrl = "https://i.ytimg.com/vi/HohJhrZFr74/maxresdefault.jpg",
+                    Price = 45.00m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Baby Blanket",
+                    Description = "A small blanket made with baby soft yarn, perfect for decorating baby's nursery. (disclaimer, do not use with children below 6 months. Please use safe sleep practices!)",
+                    ImageUrl = "https://www.anniedesigncrochet.com/wp-content/uploads/2020/04/lace-shell-crochet-baby-blanket-9.jpg",
+                    Price = 65.00m
+                }
+            );
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MermaidCraftsFE/Server/Migrations/20220831131836_productSeeding1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MermaidCraftsFE.Server.Migrations
+{
+    public partial class productSeeding1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "A teddy bear made with baby soft yarn, perfect for your child", "https://images.squarespace-cdn.com/content/v1/5e12aa9f8c03f756cbec18fc/1607712216490-NN3SB29TDH8A2RP24HLY/classic-crochet-teddy-bear.jpg?format=1000w", 50.00m, "Teddy Bear" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "A shirt made with baby soft yarn in sizes from 2t-5t", "https://i.ytimg.com/vi/HohJhrZFr74/maxresdefault.jpg", 45.00m, "Baby Shirt" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "A small blanket made with baby soft yarn, perfect for decorating baby's nursery. (disclaimer, do not use with children below 6 months. Please use safe sleep practices!)", "https://www.anniedesigncrochet.com/wp-content/uploads/2020/04/lace-shell-crochet-baby-blanket-9.jpg", 65.00m, "Baby Blanket" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
