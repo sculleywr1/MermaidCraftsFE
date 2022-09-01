9d20e1ed9126c663a897d6b83bee02a9171fdc7c
sculleywr1 <sculleywr1@gmail.com>
2022-09-01 16:21:49 -0500

    product Variant models introduced into the models

--- MermaidCraftsFE/Server/Controllers/CategoryController.cs
+++ MermaidCraftsFE/Server/Controllers/CategoryController.cs
@@ -4,12 +4,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MermaidCraftsFE.Server.Controllers
 {
+    // Gets all categories.
     [Route("api/[controller]")]
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        // Private method for _categoryService.
         private readonly ICategoryService _categoryService;
 
+        // Sets the category controller.
         public CategoryController(ICategoryService categoryService)
         {
             _categoryService = categoryService;
