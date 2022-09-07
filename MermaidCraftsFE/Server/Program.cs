82c718542c77d0ebe9563f91adda456dd1c6f169
sculleywr1 <sculleywr1@gmail.com>
2022-09-06 20:58:58 -0500

    Cors attempt 2

--- MermaidCraftsFE/Server/Program.cs
+++ MermaidCraftsFE/Server/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("https://mermaidcrafts.azurewebsites.net/");
+            policy.WithOrigins("https://mermaidcrafts.azurewebsites.net/").AllowAnyHeader().AllowAnyMethod();
         });
 });
 
