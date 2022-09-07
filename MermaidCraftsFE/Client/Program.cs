1fa5ad8be6558a3794b8a4bb10b401832c8b0dbb
sculleywr1 <sculleywr1@gmail.com>
2022-09-07 09:35:59 -0500

    Attempt 3 at CORS fix

--- MermaidCraftsFE/Client/Program.cs
+++ MermaidCraftsFE/Client/Program.cs
@@ -1,5 +1,6 @@
 using MermaidCraftsFE.Client;
 using MermaidCraftsFE.Client.Services.CategoryService;
+using MermaidCraftsFE.Client.Services.HeaderService;
 using MermaidCraftsFE.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,7 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Add a scoped host to the service. This injects the services into the program. It is a form of Dependency Injection.
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient(new HeaderDelegatingHandler()) { BaseAddress = new Uri("https://mermaidcraftsfeserverapi.azure-api.net") });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 
---
+++ MermaidCraftsFE/Client/Services/HeaderService/HeaderDelegatingHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace MermaidCraftsFE.Client.Services.HeaderService
+{
+    public class HeaderDelegatingHandler : DelegatingHandler
+    {
+        public HeaderDelegatingHandler() : base(new HttpClientHandler())
+            { }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
